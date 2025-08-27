731e5bce8f1c7fdb395674621362c28e77f25cd7
dpither <dylanpither@gmail.com>
2025-08-27 00:45:47 -0700

    Refactored select artists form, added indicator for album queue position on build queue form.

--- src/components/AlbumCard.tsx
+++ src/components/AlbumCard.tsx
@@ -3,26 +3,34 @@ import placeholder from "../assets/artist_placeholder.png";
 
 interface AlbumCardProps {
   album: SimplifiedAlbum;
-  isSelected: boolean;
+  queuePosition: number;
   onClick: () => void;
 }
 
 export default function AlbumCard({
   album,
-  isSelected,
+  queuePosition,
   onClick,
 }: AlbumCardProps) {
   function getNumSongs() {
-    if(album.total_tracks === 1) {
-      return "1 song"
+    if (album.total_tracks === 1) {
+      return "1 song";
     }
-    return album.total_tracks + " songs"
+    return album.total_tracks + " songs";
   }
+
+  const isSelected = queuePosition >= 0;
+
   return (
     <div
-      className={`${isSelected ? "border-black dark:border-white" : "border-transparent"} group flex select-none flex-col gap-2 rounded border p-2 text-sm text-black transition hover:border-black dark:text-white hover:dark:border-white`}
+      className={`${isSelected ? "border-black dark:border-white" : "border-transparent"} group relative flex select-none flex-col gap-2 rounded border p-2 text-sm text-black transition hover:border-black dark:text-white hover:dark:border-white`}
       onClick={onClick}
     >
+      {isSelected && (
+        <div className="absolute bottom-3 right-3 z-10 inline-flex size-6 items-center justify-center rounded-full bg-blue text-xs font-bold text-white">
+          {queuePosition + 1}
+        </div>
+      )}
       <img
         className="aspect-square w-full rounded-lg object-cover transition group-hover:scale-105 group-active:scale-95"
         draggable={false}
@@ -31,7 +39,8 @@ export default function AlbumCard({
       <div className="ml-1 flex flex-col text-left">
         <p className="line-clamp-2">{album.name}</p>
         <p className="line-clamp-1 text-xs text-subTextLight dark:text-subTextDark">
-          {album.release_date.substring(0, 4)} • {album.album_type} • {getNumSongs()}
+          {album.release_date.substring(0, 4)} • {album.album_type} •{" "}
+          {getNumSongs()}
         </p>
       </div>
     </div>
--- src/features/setup_steps/SelectArtistForm.tsx
+++ src/features/setup_steps/SelectArtistForm.tsx
@@ -3,21 +3,22 @@ import Icon from "../../assets/Icon";
 import tailwindConfig from "../../../tailwind.config";
 import { useState } from "react";
 import Button from "../../components/Button";
-import Artists from "../../components/Artists";
+import Spinner from "../../components/Spinner";
+import ArtistCard from "../../components/ArtistCard";
 
-type SelectArtistData = {
+interface SelectArtistData {
   selectedArtist: Artist | null;
-};
+}
 
-interface SelectArtistFormProps {
+interface SelectArtistFormProps extends SelectArtistData {
   updateSetupData: (fields: Partial<SelectArtistData>) => void;
 }
 
 export default function SelectArtistForm({
+  selectedArtist,
   updateSetupData,
 }: SelectArtistFormProps) {
   const [searchInput, setSearchInput] = useState("");
-  const [selectedId, setSelectedId] = useState<number>(-1);
   const [artists, setArtists] = useState<Artist[]>([]);
   const [isLoading, setIsLoading] = useState(false);
 
@@ -25,18 +26,18 @@ export default function SelectArtistForm({
     if (searchInput.trim() === "") {
       return;
     }
+    setArtists([]);
+    updateSetupData({ selectedArtist: null });
     searchArtists();
   }
 
-  function onSelectArtist(key: number) {
-    if (key === selectedId) {
-      setSelectedId(-1);
+  function onSelectArtist(artist: Artist) {
+    if (artist.id === selectedArtist?.id) {
       updateSetupData({ selectedArtist: null });
-      console.log("Unselecting.")
+      console.log("Unselecting " + artist.name);
     } else {
-      setSelectedId(key);
-      updateSetupData({ selectedArtist: artists[key] });
-      console.log("Selected:" + artists[key].name)
+      updateSetupData({ selectedArtist: artist });
+      console.log("Selecting " + artist.name);
     }
   }
 
@@ -59,26 +60,38 @@ export default function SelectArtistForm({
       <h1 className="text-3xl font-bold text-black dark:text-white">
         Select an Artist
       </h1>
-      <div className="relative">
-        <input
-          type="search"
-          className="w-full rounded-lg border border-black bg-black bg-opacity-0 p-4 pe-16 text-sm placeholder-subTextLight md:text-base dark:border-white dark:text-white dark:placeholder-subTextDark"
-          placeholder="Which artist intrigues you?"
-          onChange={(e) => setSearchInput(e.currentTarget.value)}
-        />
-        <div className="absolute inset-y-0 end-0 place-content-center p-2">
-          <Button onClick={search}>
-            <Icon fill={tailwindConfig.theme.colors.white} />
-          </Button>
+
+      <div className="flex min-h-0 flex-1 flex-col rounded-lg border border-black dark:border-white">
+        <div className="relative p-4">
+          <input
+            type="search"
+            className="w-full rounded-lg border border-black bg-black bg-opacity-0 p-4 pe-16 text-sm placeholder-subTextLight md:text-base dark:border-white dark:text-white dark:placeholder-subTextDark"
+            placeholder="Which artist intrigues you?"
+            onChange={(e) => setSearchInput(e.currentTarget.value)}
+          />
+          <div className="absolute inset-y-0 end-4 place-content-center p-2">
+            <Button onClick={search}>
+              <Icon fill={tailwindConfig.theme.colors.white} />
+            </Button>
+          </div>
         </div>
-      </div>
-      <div className="min-h-0 flex-1 rounded-lg border border-black dark:border-white">
-        <Artists
-          artists={artists}
-          isLoading={isLoading}
-          selectedId={selectedId}
-          onClick={onSelectArtist}
-        />
+        <div className="flex min-h-0 flex-1">
+          {isLoading && (
+            <div className="flex size-full items-center justify-center">
+              <Spinner />
+            </div>
+          )}
+          <div className="grid h-full grid-cols-2 gap-2 overflow-y-auto px-4 pb-4 sm:grid-cols-4 2xl:grid-cols-6">
+            {artists?.map((artist, i) => (
+              <ArtistCard
+                key={i}
+                artist={artist}
+                isSelected={artist.id === selectedArtist?.id}
+                onClick={() => onSelectArtist(artist)}
+              />
+            ))}
+          </div>
+        </div>
       </div>
     </div>
   );

8fb1e85701314f198c30260bb25d0413079b6357
dpither <dylanpither@gmail.com>
2026-02-17 01:43:16 -0800

    Improved swiping animations

--- src/features/swipe/TrackQueue.tsx
+++ src/features/swipe/TrackQueue.tsx
@@ -1,19 +1,25 @@
 import { AnimatePresence, motion } from "motion/react";
 import TrackCard from "../../components/TrackCard";
-import {
-	type QueueDirection,
-	usePlayerStore,
-} from "../../hooks/usePlayerStore";
+import { type QueueDecision, usePlayerStore } from "../../hooks/usePlayerStore";
 
 export default function TrackQueue() {
-	const { currentTrack, queueDirection } = usePlayerStore();
+	const {
+		currentTrack,
+		currentIndex,
+		queueDecision: queueDirection,
+		currentTimeMs,
+	} = usePlayerStore();
 
 	const variants = {
-		initial: (direction: QueueDirection) => ({
-			y: direction === "NEXT" ? "5%" : "-5%",
+		initial: (direction: QueueDecision) => ({
+			y: direction === "NEXT" || direction === "SWIPED" ? "5%" : "-5%",
 			opacity: 0,
 		}),
 		center: { y: 0, opacity: 1 },
+		exit: (direction: QueueDecision) => ({
+			y: direction === "SWIPED" ? 0 : direction === "NEXT" ? "-5%" : "5%",
+			opacity: direction === "SWIPED" ? 1 : 0,
+		}),
 	};
 
 	return (
@@ -23,12 +29,17 @@ export default function TrackQueue() {
 					animate="center"
 					className="origin-bottom"
 					custom={queueDirection}
+					exit="exit"
 					initial="initial"
 					key={currentTrack.uri}
 					transition={{ duration: 0.5, ease: "easeInOut", bounce: 0 }}
 					variants={variants}
 				>
-					<TrackCard track={currentTrack} />
+					<TrackCard
+						currentTimeMs={currentTimeMs}
+						index={currentIndex}
+						track={currentTrack}
+					/>
 				</motion.div>
 			)}
 		</AnimatePresence>
